727a7a7f97266fa6aecc8dfb2e55a54720aed9ea
root <felker.tomi@gmail.com>
2020-06-23 14:31:25 +0200

    Setup file will switch locales, so Hungarian will be the default language.

--- files/dv/dataverse-property-files/languageswitch.sh
+++ files/dv/dataverse-property-files/languageswitch.sh
@@ -8,7 +8,7 @@
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/de-AT > /opt/dv/Bundle_de.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/fr-FR > /opt/dv/Bundle_fr.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/it-IT > /opt/dv/Bundle_it.properties
-/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU > /opt/dv/Bundle_hu.properties
+/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU > /opt/dv/Bundle_en.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/astro > /opt/dv/astrophysics_hu.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/bio > /opt/dv/biomedical_hu.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/cit > /opt/dv/citation_hu.properties
@@ -17,8 +17,8 @@
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/soc > /opt/dv/socialscience_hu.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/mime_disp > /opt/dv/MimeTypeDisplay_hu.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/mime_fac > /opt/dv/MimeTypeFacets_hu.properties
-/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/roles > /opt/dv/BuiltInRoles_hu.properties
-/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US > /opt/dv/Bundle_en.properties
+/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/hu-HU/roles > /opt/dv/BuiltInRoles_en.properties
+/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US > /opt/dv/Bundle_hu.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/astro > /opt/dv/astrophysics_en.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/bio > /opt/dv/biomedical_en.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/cit > /opt/dv/citation_en.properties
@@ -27,7 +27,7 @@
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/soc > /opt/dv/socialscience_en.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/mime_disp > /opt/dv/MimeTypeDisplay_en.properties
 #/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/mime_fac > /opt/dv/MimeTypeFacets_en.properties
-/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/roles > /opt/dv/BuiltInRoles_en.properties
+/opt/dv/dataverse-property-files/lang-integrator.py /opt/dv/dataverse-property-files/en-US/roles > /opt/dv/BuiltInRoles_hu.properties
 
 if [ -n "$CV_MANAGER_URL" ]; then
     # English
@@ -61,4 +61,4 @@ if [ -n "$CV_MANAGER_URL" ]; then
     #cp -fr /opt/dv/cvmanager/templates/* /opt/glassfish4/glassfish/domains/domain1/applications/dataverse/ 
 fi
 
-curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{"locale":"hu", "title":"Magyar"},{ "locale":"en", "title":"English"}]'
+curl http://localhost:8080/api/admin/settings/:Languages -X PUT -d '[{"locale":"hu", "title":"English"},{ "locale":"en", "title":"Magyar"}]'
